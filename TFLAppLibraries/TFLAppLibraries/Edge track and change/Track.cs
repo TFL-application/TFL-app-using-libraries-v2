f8b4f63b93ea8e0c3147ba4a0e5ec312af3407b4
Naima9991 <150269222+Naima9991@users.noreply.github.com>
2024-05-09 15:35:36 +0100

    Edge, track, class

---
+++ TFLAppLibraries/TFLAppLibraries/Edge track and change/Track.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Edge_track_and_change
+{
+    public class Track : Edge
+    {
+        private bool isOpen;
+        private double delay;
+
+        // Constructor including parameters for the base class
+
+        public Track (double time, string stationFrom, string stationTo, bool isOpen = true, double delay = 0.0)
+    : base(time, stationFrom, stationTo)
+        {
+            this.isOpen = isOpen;
+            this.delay = delay;
+        }
+
+        // Override the abstract method from Edge class to calculate travel time
+        public override double GetTravelTime()
+        {
+
+
+            return base.GetTravelTime() + delay;
+
+        }
+
+        public bool GetIsOpen()
+        {
+            return isOpen;
+        }
+
+
+        public void SetIsOpen(bool isOpen)
+        {
+            this.isOpen = isOpen;
+        }
+
+        public double GetDelay()
+        {
+            return delay;
+        }
+
+
+        public void SetDelay(double delay)
+        {
+            this.delay = delay;
+        }
+
+    }
+}
